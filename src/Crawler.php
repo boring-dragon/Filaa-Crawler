eb54c49fe2f17fa43af3c0a8ff9bb54826dd3471
Mohamed jinas <j@live.mv>
2020-11-28 08:43:28 +0500

    first commit

---
+++ src/Crawler.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Jinas\Filaa;
+
+use Goutte\Client;
+use Symfony\Component\HttpClient\HttpClient;
+
+class Crawler
+{
+    protected $search;
+    protected $school;
+    protected $grade;
+    protected $subject;
+    protected $type;
+
+    protected $client;
+    protected $items;
+
+    protected const URL = "https://filaa.moe.gov.mv";
+
+    public function __construct()
+    {
+        $this->client = new Client(HttpClient::create(['verify_peer' => false, 'verify_host' => false]));
+    }
+    
+    /**
+     * setSearch
+     *
+     * @param  mixed $term
+     * @return Crawler
+     */
+    public function setSearch(string $term): Crawler
+    {
+        $this->search = $term;
+        return $this;
+    }
+    
+    /**
+     * setSchool
+     *
+     * @param  mixed $school
+     * @return Crawler
+     */
+    public function setSchool(string $school): Crawler
+    {
+        $this->school = $school;
+        return $this;
+    }
+    
+    /**
+     * setGrade
+     *
+     * @param  mixed $grade
+     * @return Crawler
+     */
+    public function setGrade(string $grade): Crawler
+    {
+        $this->grade = $grade;
+        return $this;
+    }
+    
+    /**
+     * setSubject
+     *
+     * @param  mixed $subject
+     * @return Crawler
+     */
+    public function setSubject(string $subject): Crawler
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+    
+    /**
+     * setType
+     *
+     * @param  mixed $type
+     * @return Crawler
+     */
+    public function setType(string $type): Crawler
+    {
+        $this->type = $type;
+        return $this;
+    }
+    
+    /**
+     * extract
+     *
+     * @return array
+     */
+    public function extract() : array
+    {
+        $crawler = $this->client->request('GET', sprintf(
+            $this::URL . "?search=%s&school=%s&grade=%d&subject=%s&type=%s",
+            $this->search,
+            $this->school,
+            $this->grade,
+            $this->subject,
+            $this->type
+        ));
+
+        $count = 0;
+        $crawler->filter('div[class*="thumbnail d-flex justify-content-between py-3 border-bottom flex-column flex-md-row align-items-md-center"]')->each(function ($node) use (&$count) {
+            $this->items[] = [
+                "title" => $node->filter('h2 a')->first()->text(),
+                "link" => $node->filter('h2 a')->first()->attr('href'),
+                "grade" => [
+                    "name" => $node->filter('a[title*="Grade"]')->first()->text(),
+                    "link" => $node->filter('a[title*="Grade"]')->first()->attr("href"),
+                ],
+                "subject" => [
+                    "name" => $node->filter('a[title*="Subject"]')->first()->text(),
+                    "link" => $node->filter('a[title*="Subject"]')->first()->attr("href"),
+                ],
+                "type" => [
+                    "name" => $node->filter('a[title*="Type"]')->first()->text(),
+                    "link" => $node->filter('a[title*="Type"]')->first()->attr("href"),
+                ],
+                "school" => [
+                    "name" => $node->filter('a[title*="School"]')->first()->text(),
+                    "link" => $node->filter('a[title*="School"]')->first()->attr("href"),
+                ],
+                "download_link" => $node->filter('a')->first()->attr('href'),
+            ];
+
+            $count++;
+        });
+
+        return [
+            "results" => $this->items,
+            "total_results" => $count
+        ];
+    }
+}
